d1691ffd082c14619bcc3a36b2efcdc9306f38d6
Carles <c.delriofrances@gmail.com>
2026-04-03 19:59:09 -0300

    added dynamic lang to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
+import { isValidLanguage } from "@/4-shared/helpers/isValidLanguage";
 import { allFontInstances } from "@/4-shared/lib/fonts/fontRegistry";
 import "./globals.css";
 
-export default function RootLayout({
+export default async function RootLayout({
+  params,
   children,
 }: {
   children: React.ReactNode;
+  params: Promise<{ lang?: string }>;
 }) {
   const fontVariables = allFontInstances.map((f) => f.variable).join(" ");
-
+  const realParams = await params;
+  const lang = isValidLanguage(realParams.lang) ? realParams.lang : "en";
   return (
-    <html lang="en">
+    <html lang={lang}>
       <body className={`${fontVariables} antialiased`}>{children}</body>
     </html>
   );
