0f29a476d63f6fe05802fd13354ab227ea9336ec
Carles <c.delriofrances@gmail.com>
2026-05-04 10:45:58 -0400

    fixed nested head error

---
+++ src/4-shared/lib/seo/socialProfiles.ts
@@ -0,0 +1,11 @@
+/**
+ * WeddWeb social presence URLs.
+ * Single source of truth used in both metadata (og:see_also / twitter:see_also)
+ * and JSON-LD SocialProfile structured data.
+ */
+export const WEDDWEB_SOCIAL_PROFILES = [
+  "https://www.facebook.com/weddweb",
+  "https://x.com/weddweb_com",
+  "https://www.linkedin.com/company/weddweb/",
+  "https://www.tiktok.com/@weddweb_com",
+] as const;
